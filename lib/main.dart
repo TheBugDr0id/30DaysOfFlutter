d960b8372e5592a56bb873c5ab17b3bc6e34beb7
Anuj Bhasin <anujbhasin25@gmail.com>
2022-01-23 02:54:14 +0530

    Day 3 completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_catalogue/home_page.dart';
+import 'package:flutter_catalogue/pages/home_page.dart';
+import 'package:flutter_catalogue/pages/login_page.dart';
 
 void main() {
   runApp(MyApp());
@@ -10,19 +11,22 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    
-    /*
-    double pi = 3.14;
-    bool isMale = true;
-    num temp = 45;
-
-    var day = "Saturday";
-    const pii = 3.14;
-    final pii = 3.14;
-    */ 
-
     return MaterialApp(
-      home: HomePage(),
+      // home: HomePage(),
+      themeMode: ThemeMode.light,
+      /*
+      theme: ThemeData(
+        primarySwatch: Colors.lime),
+      */
+      darkTheme: ThemeData(
+        brightness: Brightness.dark,
+      ),
+      initialRoute: "/home",
+      routes: {
+        "/": (context) => LoginPage(),
+        "/home": (context) => HomePage(),
+        "/login": (context) => LoginPage(),
+      },
     );
   }
 }
